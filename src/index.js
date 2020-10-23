6f0d88b7feb9c14927484bff2ac24404211efec4
Ананичев Леонид Дмитриевич <ananichev.l.dm@sberbank.ru>
2020-10-23 16:50:34 +0300

    feat: add toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function toReadable (number) {
-  
-}
+const hundred = 'hundred';
+
+const dg = ['zero', 'one', 'two', 'three', 'four',
+    'five', 'six', 'seven', 'eight', 'nine'];
+
+const tn =
+    ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'];
+
+const tw = ['twenty', 'thirty', 'forty', 'fifty',
+    'sixty', 'seventy', 'eighty', 'ninety'];
+
+module.exports = function toReadable(number) {
+    let result = '';
+
+    let hundreds = parseInt(number / 100);
+
+    result += (dg[hundreds] === 'zero') ? '' : dg[hundreds] + ' ' + hundred + ' ';
+
+    let dozens = number % 100;
+
+    if (dozens < 10) {
+        if (dozens === 0 && result === '') {
+            result += dg[dozens];
+        } else if (dozens !== 0) {
+            result += dg[dozens];
+        }
+    } else if (dozens < 20) {
+        result += tn[parseInt(dozens % 10)];
+    } else {
+        result += tw[parseInt(dozens / 10) - 2];
+        result += (dozens % 10 === 0) ? '' : ' ' + dg[parseInt(dozens % 10)];
+    }
+
+    console.log(result);
+
+    return result.trim();
+};
